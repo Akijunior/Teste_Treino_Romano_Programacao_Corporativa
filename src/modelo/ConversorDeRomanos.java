713ac1ade59e6f9b510a901868d8e523d532bd12
Akijunior <suitsu19@gmail.com>
2017-07-13 15:25:19 -0300

    Encorpando o codigo fonte.

--- src/modelo/ConversorDeRomanos.java
+++ src/modelo/ConversorDeRomanos.java
@@ -1,6 +1,23 @@
 package modelo;
 
 public class ConversorDeRomanos {
+	
+	public double ConverterRomanoParaInteiro(String valorRomano) {
+		
+		double valorASerRetornado = 0;
+		
+		for (int i = 0; i < valorRomano.length; i++) {
+			
+		}
+	}
+	
+	public double ValidadorDeLetras(String palavra) {
+		
+		for (int i = 0; i < palavra.length; i++) {
+			if (palavra[i]i )
+			
+		}
+	}
 
 
 }
